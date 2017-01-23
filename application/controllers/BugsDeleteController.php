b5619aa0286a310ecc2a73b00c257b9940051e9b
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2017-01-23 11:16:40 +0200

    * removed non-necessary controllers

--- application/controllers/BugsDeleteController.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("application/models/dao/Bugs.php");
-
-class BugsDeleteController extends Controller {
-    public function run() {
-        $obj = new Bugs();
-        $obj->delete($_POST["id"]);
-    }
-}
--- application/controllers/PanelDeleteController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class PanelDeleteController extends Controller {
-    public function run() {
-        $users = new Panels();
-        $users->delete($_POST["id"]);
-    }
-}
