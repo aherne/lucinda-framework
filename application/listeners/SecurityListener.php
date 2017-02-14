6d65cfd1a77c06b96f631b9efd5e5cafd2698198
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2017-02-14 17:01:53 +0200

    * removed unnecessary files

--- application/listeners/SecurityListener.php
+++ application/listeners/SecurityListener.php
@@ -1,65 +1,68 @@
 <?php
 require_once("application/models/DAOLocator.php");
+require_once("libraries/php-security-api/loader.php");
 
-// TODO: follow a strict exceptions policy (throwing an unchecked exception in this section causes undefined behavior)
 class SecurityListener extends RequestListener {
 	private $daoLocator;
 	private $persistenceDrivers = array();
-	private $userID;
-	
+
 	public function run() {
 		$this->setDAOLocator();
 		$this->setPersistenceDrivers();
 		$this->setUserID();
-		
+
 		$this->csrf();
 		$this->authenticate();
 		$this->authorize();
 	}
-	
+
 	private function setDAOLocator() {
 		$this->daoLocator = new DAOLocator($this->application->getXML());
 	}
-	
+
 	private function setPersistenceDrivers() {
 		$xml = $this->application->getXML()->security->persistence;
 		if(empty($xml)) return; // it is allowed for elements to not persist
-		
+
 		if($xml->session) {
-			require_once("application/models/persistence/SessionPersistendeDriverWrapper.php");
+			require_once("application/models/persistence/SessionPersistenceDriverWrapper.php");
 			$wrapper = new SessionPersistenceDriverWrapper($xml->session);
 			$this->persistenceDrivers[] = $wrapper->getDriver();
 		}
-		
+
 		if($xml->remember_me) {
-			require_once("application/models/persistence/RememberMePersistendeDriverWrapper.php");
+			require_once("application/models/persistence/RememberMePersistenceDriverWrapper.php");
 			$wrapper = new RememberMePersistenceDriverWrapper($xml->remember_me);
 			$this->persistenceDrivers[] = $wrapper->getDriver();
 		}
-				
+
 		if($xml->token) {
-			require_once("application/models/persistence/TokenPersistendeDriverWrapper.php");
+			require_once("application/models/persistence/TokenPersistenceDriverWrapper.php");
 			$wrapper = new TokenPersistenceDriverWrapper($xml->token);
 			$this->persistenceDrivers[] = $wrapper->getDriver();
 		}
 	}
-	
+
 	private function setUserID() {
+		$userID = null;
 		foreach($this->persistenceDrivers as $persistenceDriver) {
-			$this->userID = $persistenceDriver->load();
-			break;
+			$userID = $persistenceDriver->load();
+			if($userID) {
+				break;
+			}
 		}
+		$this->request->setAttribute("user_id", $userID);
 	}
-	
+
 	private function csrf() {
-		
+
 	}
-	
+
 	// TODO: tokens do not support redirection
 	private function authenticate() {
 		$xml = $this->application->getXML()->security->authentication;
 		if(empty($xml)) throw new ServletApplicationException("Entry missing in configuration.xml: security.authentication");
-		
+
 		if($xml->form) {
 			require_once("application/models/authentication/FormAuthenticationWrapper.php");
 			new FormAuthenticationWrapper($xml->form, $this->request->getAttribute("page_url"), $this->persistenceDrivers, $this->daoLocator);
@@ -69,18 +72,18 @@ class SecurityListener extends RequestListener {
 			new Oauth2AuthenticationWrapper($xml->oauth2, $this->request->getAttribute("page_url"), $this->persistenceDrivers, $this->daoLocator);
 		}
 	}
-	
+
 	private function authorize() {
 		$xml = $this->application->getXML()->security->authorization;
 		if(empty($xml)) throw new ServletApplicationException("Entry missing in configuration.xml: security.authentication");
-		
+
 		if($xml->by_route) {
 			require_once("application/models/authorization/XMLAuthorizationWrapper.php");
-			new XMLAuthorizationWrapper($this->application->getXML(), $this->request->getAttribute("page_url"), $this->userID);
+			new XMLAuthorizationWrapper($this->application->getXML(), $this->request->getAttribute("page_url"), $this->request->getAttribute("user_id"));
 		}
 		if($xml->by_dao) {
 			require_once("application/models/authorization/DAOAuthorizationWrapper.php");
-			new DAOAuthorizationWrapper($xml->oauth2, $this->request->getAttribute("page_url"), $this->userID, $this->daoLocator);
+			new DAOAuthorizationWrapper($xml->by_dao, $this->request->getAttribute("page_url"), $this->request->getAttribute("user_id"), $this->daoLocator);
 		}
 	}
 }
--- application/models/DAOLocator.php
+++ application/models/DAOLocator.php
@@ -1,24 +1,25 @@
 <?php
 class DAOLocator {
 	private $daoPath;
-	
+
 	public function __construct(SimpleXMLElement $xml) {
 		$path = (string) $xml->application->paths->dao;
 		if(!$path) throw new ServletApplicationException("Tag application.paths.dao is mandatory!");
 		$this->daoPath = $path;
 	}
-	
+
 	public function locate(SimpleXMLElement $xml, $parameterName, $parentClassName) {
 		$dao = (string) $xml[$parameterName];
-		if(!$dao) throw new ServletApplicationException("'page_dao' attribute of 'by_dao' tag is missing!");
-		
+		if(!$dao) throw new ServletApplicationException("'$parameterName' attribute is missing!");
+
 		// load file
 		$daoFile = $this->daoPath."/".$dao.".php";
 		if(!file_exists($daoFile)) throw new ServletApplicationException("DAO file not found: ".$daoFile."!");
 		require_once($daoFile);
-		
+
 		// locate class
-		if(!($dao instanceof $parentClassName)) throw new ServletApplicationException($dao." must be instance of ".$parentClassName."!");
-		return new $dao();
+		$daoObject = new $dao();
+		if(!($daoObject instanceof $parentClassName)) throw new ServletApplicationException($dao." must be instance of ".$parentClassName."!");
+		return $daoObject;
 	}
 }
