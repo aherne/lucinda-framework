57f1fccfc433c899613dab7336b9388aeb0b15c1
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2017-02-14 18:22:26 +0200

    + bugfix & documentation

--- application/listeners/SecurityListener.php
+++ application/listeners/SecurityListener.php
@@ -2,10 +2,14 @@
 require_once("application/models/DAOLocator.php");
 require_once("libraries/php-security-api/loader.php");
 
+/**
+ * Reads XML "security" tag for settings that define how authentication/authorization/persistence will be performed inside your application.
+ * NOTE: this is not needed if your application serves only public content!
+ */
 class SecurityListener extends RequestListener {
 	private $daoLocator;
 	private $persistenceDrivers = array();
-
+	
 	public function run() {
 		$this->setDAOLocator();
 		$this->setPersistenceDrivers();
@@ -16,10 +20,22 @@ class SecurityListener extends RequestListener {
 		$this->authorize();
 	}
 
+	/**
+	 * Starts DAO locator helper
+	 */
 	private function setDAOLocator() {
 		$this->daoLocator = new DAOLocator($this->application->getXML());
 	}
 
+	/**
+	 * Detects drivers where user unique identifier will be persisted to across requests based on contents of security.persistence tag.
+	 * 
+	 * Supported drivers:
+	 * 1. session: user id will persist as a session parameter 
+	 * 2. remember_me: user id will persist as a crypted cookie
+	 * 3. token: user id will become available 
+	 * @throws ServletApplicationException When settings were improperly written.
+	 */
 	private function setPersistenceDrivers() {
 		$xml = $this->application->getXML()->security->persistence;
 		if(empty($xml)) return; // it is allowed for elements to not persist
@@ -43,6 +59,9 @@ class SecurityListener extends RequestListener {
 		}
 	}
 
+	/**
+	 * Detects user unique identifier from persistence drivers and saves it to "user_id" request attribute.
+	 */
 	private function setUserID() {
 		$userID = null;
 		foreach($this->persistenceDrivers as $persistenceDriver) {
@@ -58,7 +77,15 @@ class SecurityListener extends RequestListener {
 
 	}
 
-	// TODO: tokens do not support redirection
+	/**
+	 * Performs authentication based on drivers defined in security.authentication tag.
+	 * 
+	 * Supported drivers:
+	 * - form: user will be authenticated based on a login form
+	 * - oauth2: user will be authenticated based on an oauth2 driver (supported: google, facebook)
+	 * 
+	 * @throws ServletApplicationException If XML settings are incorrect
+	 */
 	private function authenticate() {
 		$xml = $this->application->getXML()->security->authentication;
 		if(empty($xml)) throw new ServletApplicationException("Entry missing in configuration.xml: security.authentication");
@@ -73,6 +100,15 @@ class SecurityListener extends RequestListener {
 		}
 	}
 
+	/**
+	 * Performs authorization of user to resource based on drivers defined in security.authorization tag.
+	 * 
+	 * Supported drivers:
+	 * - by_route: user will be authorized based on logged in status and "access" property of routes.route tags set in XML (can be ROLE_USER / ROLE_GUEST) 
+	 * - by_dao: user will be authorized based on logged in user and page accessed via mandatory user-defined DAOs (UserAuthorizationDAO, PageAuthorizationDAO)
+	 * 
+	 * @throws ServletApplicationException If XML settings are incorrect
+	 */
 	private function authorize() {
 		$xml = $this->application->getXML()->security->authorization;
 		if(empty($xml)) throw new ServletApplicationException("Entry missing in configuration.xml: security.authentication");
