3fdc68c4f46c7e9ec4f5880ef06bb7e644274c92
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2017-02-17 17:38:14 +0200

    + documentation & refactoring

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Lucinda Framework API
 
 This is the api that's going to be used by lucinda framework.
+
+TODOS:
+- @ ServletsAPI: getSecurityContext() @ Request object; getValidPage(); getPathParameters()
---
+++ application/models/security/CsrfTokenWrapper.php
@@ -0,0 +1,41 @@
+<?php
+
+class CsrfTokenWrapper {
+	const DEFAULT_EXPIRATION = 10*60;
+	
+	private $secret;
+	private $expiration;
+	
+	private $token;
+	
+	public function __construct(SimpleXMLElement $xml) {
+		// sets secret
+		$secret = (string) $xml["secret"];
+		if(!$secret) throw new ServletApplicationException("'secret' attribute not set in security.csrf tag");
+		
+		// sets token
+		$this->token = new SynchronizerToken($_SERVER["REMOTE_ADDR"], $secret);
+		
+		// sets expiration
+		$expiration = (string) $xml["expiration"];
+		if(!$expiration) $expiration = self::DEFAULT_EXPIRATION;
+		$this->expiration = $expiration;		
+	}
+	
+	public function generate($userID) {
+		return $this->token->encode($userID, $this->expiration);
+	}
+	
+	public function isValid($token, $userID) {
+		try {
+			$tokenUserID = $this->token->decode($token);
+			if($tokenUserID == $userID) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch(Exception $e) {
+			return false;
+		}
+	}
+}
