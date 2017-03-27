dfd1305d1fd0f8e47d3b9421fab122ded11e3e12
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2017-03-27 17:12:39 +0300

    * some changes

--- application/models/ErrorManager.php
+++ application/models/ErrorManager.php
@@ -1,14 +1,10 @@
 <?php
-// setup conservative reporting
 require_once("libraries/php-errors-api/loader.php");
 require_once("libraries/php-logging-api/src/FileLogger.php");
 require_once("application/models/HtmlRenderer.php");
+
 $errorHandler = new ErrorHandler();
 $errorHandler->addReporter(new FileLogger("errors"));
 $errorHandler->setRenderer(new HtmlRenderer());
 PHPException::setErrorHandler($errorHandler);
 set_exception_handler(array($errorHandler,"handle"));
-
-// starts MVC api
-require_once("libraries/php-servlets-api/loader.php");
-new FrontController();
