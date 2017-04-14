2e0d6f83ce2da4ed9d63c701522bb252e385d18c
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2017-04-14 08:52:05 +0300

    + unified renderers

--- application/models/errors/renderers/JsonRenderer.php
+++ application/models/errors/renderers/JsonRenderer.php
@@ -24,7 +24,24 @@ class JsonRenderer implements ErrorRenderer {
 	public function render($exception) {
 		header_remove();
 		header("Content-Type: application/json; charset=".$this->charset);
-		if($exception instanceof PathNotFoundException) {
+		if($exception instanceof SecurityPacket) {
+			switch($exception->getStatus()) {
+				case "unauthorized":
+					header("HTTP/1.1 401 Unauthorized");
+					echo json_encode(array("status"=>"unauthorized","body"=>"", "callback"=>$exception->getCallback()));
+				case "forbidden":
+					header("HTTP/1.1 403 Forbidden");
+					echo json_encode(array("status"=>"forbidden","body"=>"", "callback"=>$exception->getCallback()));
+				case "not_found":
+					header("HTTP/1.1 404 Not found");
+					echo json_encode(array("status"=>"not_found","body"=>"", "callback"=>$exception->getCallback()));
+				case "login_ok":
+					echo json_encode(array("status"=>"login_ok","body"=>"", "callback"=>$exception->getCallback(), "token"=>$exception->getAccessToken()));
+				default:
+					echo json_encode(array("status"=>($exception->getStatus()?$exception->getStatus():"redirect"),"body"=>"", "callback"=>$exception->getCallback()));
+					break;
+			}
+		} else if($exception instanceof PathNotFoundException) {
 			header("HTTP/1.1 404 Not found");
 			echo json_encode(array("status"=>"not_found","body"=>""));
 		} else {
