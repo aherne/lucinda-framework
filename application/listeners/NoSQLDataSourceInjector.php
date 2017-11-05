5fe9e03e235835469137fc392d4b334c514f3c6c
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2017-11-05 13:06:45 +0200

    * adapted framework to NoSQL API changes

--- application/listeners/NoSQLDataSourceInjector.php
+++ application/listeners/NoSQLDataSourceInjector.php
@@ -73,55 +73,128 @@ class NoSQLDataSourceInjector extends ApplicationListener {
 		if(!$driver) throw new ServletException("Child tag <driver> is mandatory for <server> tags!");
 		switch($driver) {
 			case "couchbase":
+				$host = (string) $databaseInfo["host"];
+				$userName = (string) $databaseInfo["username"];
+				$password = (string) $databaseInfo["password"];
+				$bucket = (string) $databaseInfo["bucket_name"];
+				if(!$host || !$userName || !$password || !$bucket) throw new ServletException("For COUCHBASE driver following attributes are mandatory: host, username, password, bucket_name");
+				
 				require_once("libraries/php-nosql-data-access-api/src/CouchbaseDriver.php");
 				
 				$dataSource = new CouchbaseDataSource();
-				$dataSource->setHost((string) $databaseInfo["host"]);
-				$dataSource->setPort((string) $databaseInfo["port"]);
-				$dataSource->setUserName((string) $databaseInfo["username"]);
-				$dataSource->setPassword((string) $databaseInfo["password"]);
-				
-				$bucket = (string) $databaseInfo["bucket"];
-				if($bucket) {
-					$dataSource->setBucketInfo($bucket, (string) $databaseInfo["bucket_password"]);
-				}
+				$dataSource->setHost($host);
+				$dataSource->setAuthenticationInfo($userName, $password);				
+				$dataSource->setBucketInfo($bucket, (string) $databaseInfo["bucket_password"]);
 				return $dataSource;
 				break;
 			case "memcache":
+				$temp = (string) $databaseInfo["host"];
+				if(!$temp) throw new ServletException("For MEMCACHE driver attribute 'host' is mandatory");
+				
 				require_once("libraries/php-nosql-data-access-api/src/MemcacheDriver.php");
 				
 				$dataSource = new MemcacheDataSource();
-				$dataSource->setHost((string) $databaseInfo["host"]);
-				$dataSource->setPort((string) $databaseInfo["port"]);
+				$hostsAndPorts = $this->getHostsAndPorts($temp);
+				foreach($hostsAndPorts as $host=>$port){
+					if(!$port) {
+						$dataSource->addServer($host);
+					} else {
+						$dataSource->addServer($host, $port);
+					}
+				}
+				
+				$timeout= (string) $databaseInfo["timeout"];
+				if($timeout) {
+					$dataSource->setTimeout($timeout);
+				}
+				
+				$persistent = (string) $databaseInfo["persistent"];
+				if($persistent) {
+					$dataSource->setPersistent();
+				}
+				
 				return $dataSource;
 			case "memcached":
+				$temp = (string) $databaseInfo["host"];
+				if(!$temp) throw new ServletException("For MEMCACHED driver attribute 'host' is mandatory");
+				
 				require_once("libraries/php-nosql-data-access-api/src/MemcachedDriver.php");
 				
-				$dataSource = new MemcachedDataSource();
-				$dataSource->setHost((string) $databaseInfo["host"]);
-				$dataSource->setPort((string) $databaseInfo["port"]);
+				$dataSource = new MemcachedDataSource();				
+				$hostsAndPorts = $this->getHostsAndPorts($temp);
+				foreach($hostsAndPorts as $host=>$port){
+					if(!$port) {
+						$dataSource->addServer($host);
+					} else {
+						$dataSource->addServer($host, $port);
+					}
+				}
+				
+				$timeout= (string) $databaseInfo["timeout"];
+				if($timeout) {
+					$dataSource->setTimeout($timeout);
+				}
+				
+				$persistent = (string) $databaseInfo["persistent"];
+				if($persistent) {
+					$dataSource->setPersistent();
+				}
+				
 				return $dataSource;
 			case "redis":
+				$temp = (string) $databaseInfo["host"];
+				if(!$temp) throw new ServletException("For REDIS driver attribute 'host' is mandatory");
+				
 				require_once("libraries/php-nosql-data-access-api/src/RedisDriver.php");
 				
 				$dataSource = new RedisDataSource();
-				$dataSource->setHost((string) $databaseInfo["host"]);
-				$dataSource->setPort((string) $databaseInfo["port"]);
+				$hostsAndPorts = $this->getHostsAndPorts($temp);
+				foreach($hostsAndPorts as $host=>$port){
+					if(!$port) {
+						$dataSource->addServer($host);
+					} else {
+						$dataSource->addServer($host, $port);
+					}
+				}
+				
+				$timeout= (string) $databaseInfo["timeout"];
+				if($timeout) {
+					$dataSource->setTimeout($timeout);
+				}
+								
+				$persistent = (string) $databaseInfo["persistent"];
+				if($persistent) {
+					$dataSource->setPersistent();
+				}
+				
 				return $dataSource;
 			case "apc":
 				require_once("libraries/php-nosql-data-access-api/src/APCDriver.php");
 				
-				$dataSource = new APCDataSource();
-				return $dataSource;
+				return new APCDataSource();
 			case "apcu":
 				require_once("libraries/php-nosql-data-access-api/src/APCuDriver.php");
 				
-				$dataSource = new APCuDataSource();
-				return $dataSource;
+				return new APCuDataSource();
 			default:
 				throw new ServletException("Nosql driver not supported: ".$driver);
 				break;
 		}
 		return $dataSource;
+	}
+	
+	private function getHostsAndPorts($temp) {
+		$output = array();
+		$hosts = explode(",",$temp);
+		foreach($hosts as $hostAndPort) {
+			$hostAndPort = trim($hostAndPort);
+			$position = strpos($hostAndPort,":");
+			if($position!==false) {
+				$output[substr($hostAndPort, 0, $position)]=substr($hostAndPort,$position+1);
+			} else {
+				$output[$hostAndPort]=null;
+			}
+		}
+		return $output;
 	}
 }
