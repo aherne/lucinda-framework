c72b9338668031fdd065fc2182791db4151a71f8
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2017-11-12 10:22:45 +0200

    * renamed repo

--- test/security/DAOAuthorizationWrapper.php
+++ test/security/DAOAuthorizationWrapper.php
@@ -15,7 +15,7 @@ $xmlString = '
 		</xml>';
 $xml = simplexml_load_string($xmlString);
 require_once(str_replace("test/security","src/security", __FILE__));
-require_once(dirname(dirname(__DIR__))."/vendor/lucinda/servlets/src/exceptions/ApplicationException.php");
+require_once(dirname(dirname(__DIR__))."/vendor/lucinda/mvc/src/exceptions/ApplicationException.php");
 require_once(dirname(dirname(__DIR__))."/vendor/lucinda/security/src/authorization/DAOAuthorization.php");
 require_once(dirname(dirname(__DIR__))."/src/security/DAOLocator.php");
 $_SERVER["REQUEST_METHOD"] = "GET";
