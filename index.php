0fca95b2af74811dafdfd915fae999720ea6b64f
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2018-02-01 13:36:39 +0200

    detached PathNotFoundException from handling

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 // starts MVC api
 require_once("vendor/lucinda/mvc/loader.php");
-new FrontController();
+try {
+	new FrontController();
+} catch(PathNotFoundException $exception) {
+	// below applies only if display format is HTML
+	header("HTTP/1.0 404 Not Found");
+	header('Content-Type: text/html; charset=UTF-8');
+	require_once("application/views/404.php");
+}
