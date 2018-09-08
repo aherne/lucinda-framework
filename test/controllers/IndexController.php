cf35d38ad0fbcbb8e8b299c9cd87b227e7544d31
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2018-09-08 13:22:21 +0300

    cleared unnecessary code

--- test/controllers/IndexController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class IndexController extends Controller {
-    public function run() {
-        $this->response->setView("index");
-        $this->response->setAttribute("user", "member");
-    }
-}
