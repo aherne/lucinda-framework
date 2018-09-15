11a8f28e08159b217625d8e2e4b0ed1026822a38
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2018-09-15 16:21:25 +0300

    * minor changes

--- application/models/errors/reporters/LogReporter.php
+++ application/models/errors/reporters/LogReporter.php
@@ -1,4 +1,5 @@
 <?php
+require_once("vendor/lucinda/logging/loader.php");
 require_once("vendor/lucinda/errors-mvc/src/ErrorReporter.php");
 require_once("vendor/lucinda/errors-mvc/src/Request.php");
 require_once("vendor/lucinda/errors-mvc/src/ErrorType.php");
