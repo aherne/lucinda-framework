e97bc3bfd4c8c66a07e6a68c509e25e45dcfabbb
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2018-10-20 16:38:10 +0300

    * renamed xmls

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ if(!$environment) die("Value of environment variable 'ENVIRONMENT' could not be 
 require_once("vendor/lucinda/errors-mvc/src/FrontController.php");
 require_once("application/models/errors/EmergencyHandler.php");
 require_once("application/models/errors/reporters/LogReporter.php");
-new Lucinda\MVC\STDERR\FrontController("errors.xml", $environment, __DIR__, new EmergencyHandler());
+new Lucinda\MVC\STDERR\FrontController("stderr.xml", $environment, __DIR__, new EmergencyHandler());
 
 // takes control of STDOUT
 require_once("vendor/lucinda/mvc/loader.php");
-new Lucinda\MVC\STDOUT\FrontController("configuration.xml");
+new Lucinda\MVC\STDOUT\FrontController("stdout.xml");
