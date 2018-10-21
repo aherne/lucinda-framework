3b0e97cbe05c80c7a075cee6eb0ecfc3b2c7d0fc
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2018-10-21 09:06:01 +0300

    * removed performance-unfriendly line

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ if(!$environment) die("Value of environment variable 'ENVIRONMENT' could not be 
 // takes control of STDERR
 require_once("vendor/lucinda/errors-mvc/src/FrontController.php");
 require_once("application/models/errors/EmergencyHandler.php");
-require_once("application/models/errors/reporters/LogReporter.php");
 new Lucinda\MVC\STDERR\FrontController("stderr.xml", $environment, __DIR__, new EmergencyHandler());
 
 // takes control of STDOUT
