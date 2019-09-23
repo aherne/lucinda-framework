673de0734ff26b71411a4ec4e144d52ed6532294
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2019-09-23 08:17:18 +0300

    reformatting of error messages

--- application/views/debug.php
+++ application/views/debug.php
@@ -1,14 +1,19 @@
 <html>
-	<head>
-		<title>Bug Report</title>
-	</head>
-	<body>
-		<h1>ERROR</h1>
-		
-		<h2>Message</h2>
-		<?php echo $exception->getMessage(); ?>
-		
-		<h2>Trace</h2>
-		<?php echo nl2br($exception->getTraceAsString()); ?>
-	</body>
+    <head>
+        <title>Bug Report</title>
+    </head>
+    <body>
+        <h1>ERROR</h1>
+        
+        <h2>Message</h2>
+        <p><?php echo nl2br($exception->getMessage()); ?></p>
+        
+        <?php if($exception instanceof \Lucinda\SQL\StatementException) { ?>
+        <h2>Query</h2>
+        <p><?php echo nl2br($exception->getQuery()); ?></p>
+        <?php } ?>
+        
+        <h2>Trace</h2>
+        <p><?php echo nl2br($exception->getTraceAsString()); ?></p>
+    </body>
 </html>
