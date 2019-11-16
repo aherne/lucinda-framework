7d9d5b8bd2af641ffeedad964d83edb280b82d81
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2019-11-16 11:16:52 +0200

    Made STDERR MVC structure mirror that of STDOUT MVC

--- application/listeners/ErrorListener.php
+++ application/listeners/ErrorListener.php
@@ -11,6 +11,8 @@ class ErrorListener extends \Lucinda\MVC\STDOUT\RequestListener
     public function run()
     {
         $handler = \Lucinda\MVC\STDERR\PHPException::getErrorHandler();
-        $handler->setContentType($this->application->formats($this->request->getValidator()->getFormat())->getContentType());
+        if ($handler instanceof \Lucinda\MVC\STDERR\FrontController) {
+            $handler->setDisplayFormat($this->request->getValidator()->getFormat());
+        }
     }
 }
