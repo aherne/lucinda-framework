5ee8fcc94e415fa9206ad50a753908f264ac65e6
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2020-02-14 16:28:17 +0200

    customization of template error reporting

--- application/resolvers/HtmlResolver.php
+++ application/resolvers/HtmlResolver.php
@@ -2,8 +2,13 @@
 /**
  * View resolver for HTML response format to be used whenever developers do not desire HTML templating (NOT RECOMMENDED!)
  */
-class HtmlResolver extends \Lucinda\MVC\STDOUT\ViewResolver
+class HtmlResolver extends \Lucinda\MVC\STDOUT\ViewResolver implements \Lucinda\MVC\STDERR\ErrorHandler
 {
+    /**
+     * @var \Lucinda\MVC\STDERR\ErrorHandler
+     */
+    private $defaultErrorHandler;
+
     /**
      * {@inheritDoc}
      * @see \Lucinda\MVC\STDOUT\ViewResolver::getContent()
@@ -18,13 +23,37 @@ class HtmlResolver extends \Lucinda\MVC\STDOUT\ViewResolver
             if (!file_exists($view)) {
                 throw new Lucinda\MVC\STDOUT\ServletException("View file not found: ".$view);
             }
-            
-            
+
+            $this->defaultErrorHandler = \Lucinda\MVC\STDERR\PHPException::getErrorHandler();
+
+            // take control of error reporting
+            \Lucinda\MVC\STDERR\PHPException::setErrorHandler($this);
+            set_exception_handler(array($this,"handle"));
+
+            // compiles PHP file into output buffer
             ob_start();
             require($view);
             $output = ob_get_contents();
             ob_end_clean();
+
+            // restore default error handler
+            \Lucinda\MVC\STDERR\PHPException::setErrorHandler($this->defaultErrorHandler);
+            set_exception_handler(array($this->defaultErrorHandler,"handle"));
         }
         return $output;
+    }
+
+    /**
+     * Handles errors by delegating to STDOUT MVC API
+     *
+     * @param \Exception $exception Encapsulates error information.
+     */
+    public function handle($exception)
+    {
+        // close output buffer
+        ob_end_clean();
+
+        // delegate handling to STDERR MVC API
+        $this->defaultErrorHandler->handle($exception);
     }
 }
