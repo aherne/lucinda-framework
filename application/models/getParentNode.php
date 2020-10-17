ee3112dbc2e1b2f12db8cc107b9229de074aae3b
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2020-10-17 08:56:46 +0300

    bugfix

--- application/models/getParentNode.php
+++ application/models/getParentNode.php
@@ -2,11 +2,11 @@
 /**
  * Gets parent node to that identified by tag name
  * 
- * @param Lucinda\STDOUT\Application $application
+ * @param Lucinda\STDOUT\Application|Lucinda\STDERR\Application $application
  * @param string $tagName
  * @return SimpleXMLElement
  */
-function getParentNode(Lucinda\STDOUT\Application $application, string $tagName): SimpleXMLElement
+function getParentNode($application, string $tagName): SimpleXMLElement
 {
     $parents = $application->getTag($tagName)->xpath("..");
     return $parents[0]??new SimpleXMLElement("<xml></xml>");
--- application/renderers/HtmlRenderer.php
+++ application/renderers/HtmlRenderer.php
@@ -1,4 +1,6 @@
 <?php
+require_once("application/models/getParentNode.php");
+
 use Lucinda\Templating\Wrapper;
 
 /**
@@ -26,8 +28,7 @@ class HtmlRenderer extends \Lucinda\STDERR\ViewResolver implements \Lucinda\STDE
             $this->defaultErrorHandler = \Lucinda\STDERR\PHPException::getErrorHandler();
             
             // converts view language to PHP
-            $parents = $this->application->getTag("templating")->xpath("..");
-            $wrapper = new Wrapper($parents[0]??new SimpleXMLElement("<xml></xml>"));
+            $wrapper = new Wrapper(getParentNode($this->application, "templating"));
             
             // take control of error handling
             \Lucinda\STDERR\PHPException::setErrorHandler($this);
