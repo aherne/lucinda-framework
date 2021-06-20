f2f1a107a02102a4c4457ba6106e15379b72e8f1
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2021-06-20 10:07:32 +0300

    progress

---
+++ src/DAO/NoSQLMigrationCache.php
@@ -0,0 +1,84 @@
+<?php
+namespace Lucinda\Project\DAO;
+
+use Lucinda\NoSQL\ConnectionSingleton;
+use Lucinda\Migration\Status;
+
+/**
+ * Saves migration progress in an NoSQL key-value store
+ */
+class NoSQLMigrationCache implements \Lucinda\Migration\Cache
+{
+    private $keyName;
+    private $connection;
+    
+    /**
+     * Sets key name
+     *
+     * @param string $keyName
+     */
+    public function __construct(string $keyName = "migrations")
+    {
+        $this->keyName = $keyName;
+        $this->connection = ConnectionSingleton::getInstance();
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Lucinda\Migration\Cache::exists()
+     */
+    public function exists(): bool
+    {
+        return $this->connection->contains($this->keyName);
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Lucinda\Migration\Cache::create()
+     */
+    public function create(): void
+    {
+        $this->connection->set($this->keyName, json_encode([]));
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Lucinda\Migration\Cache::read()
+     */
+    public function read(): array
+    {
+        $items = json_decode($this->connection->get($this->keyName), true);
+        $results = [];
+        foreach ($items as $className => $details) {
+            $results[$className] = ($details["is_successful"]?Status::PASSED:Status::FAILED);   
+        }
+        return $results;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Lucinda\Migration\Cache::add()
+     */
+    public function add(string $className, int $statusCode): void
+    {
+        $isSuccessful = ($statusCode==Status::PASSED);        
+        $items = json_decode($this->connection->get($this->keyName), true);
+        if (isset($items[$className]) && ($items[$className]["is_successful"] == $isSuccessful)) {
+            return;
+        }
+        $items[$className] = ["is_successful"=>$isSuccessful, "date"=>date("Y-m-d H:i:s")];
+        $this->connection->set($this->keyName, json_encode($items));
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Lucinda\Migration\Cache::remove()
+     */
+    public function remove(string $className): void
+    {
+        $items = json_decode($this->connection->get($this->keyName), true);
+        unset($items[$className]);
+        $this->connection->set($this->keyName, json_encode($items));
+    }
+}
+
---
+++ src/DAO/SQLLoginThrottler.php
@@ -0,0 +1,67 @@
+<?php
+namespace Lucinda\Project\DAO;
+
+use Lucinda\Framework\AbstractLoginThrottler;
+use Lucinda\WebSecurity\Request;
+use Lucinda\WebSecurity\Authentication\Form\LoginThrottler;
+
+/**
+ * Login throttler that uses a "user_logins" SQL table (created beforehand) to check logins history. Create table statement if MySQL:
+ * 
+    CREATE TABLE user_logins (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    ip VARCHAR(45) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    attempts BIGINT UNSIGNED NOT NULL default 0,
+    penalty_expiration DATETIME DEFAULT NULL,
+    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(id),
+    UNIQUE(ip, username)
+    ) Engine=InnoDB
+ *
+ */
+class SQLLoginThrottler extends AbstractLoginThrottler
+{
+    const TABLE_NAME = "user_logins";
+    private $found;
+    
+    /**
+     * {@inheritDoc}
+     * @see \Lucinda\WebSecurity\Authentication\Form\LoginThrottler::setCurrentStatus()
+     */
+    protected function setCurrentStatus(): void
+    {
+        $row = SQL("SELECT attempts, penalty_expiration FROM ".self::TABLE_NAME." WHERE ip=:ip AND username=:username", [
+            ":ip"=>$this->request->getIpAddress(),
+            ":username"=>$this->userName
+        ])->toRow();
+        if (!empty($row)) {
+            $this->attempts = $row["attempts"];
+            $this->penaltyExpiration = $row["penalty_expiration"];
+            $this->found = true;
+        }
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Lucinda\Framework\AbstractLoginThrottler::persist()
+     */
+    protected function persist(): void
+    {
+        if (!$this->found) {
+            SQL("INSERT INTO ".self::TABLE_NAME." (ip, username, attempts, penalty_expiration) VALUES (:ip, :username, :attempts, :penalty_expiration)", [
+                ":ip"=>$this->request->getIpAddress(),
+                ":username"=>$this->userName,
+                ":attempts"=>$this->attempts,
+                ":penalty_expiration"=>$this->penaltyExpiration
+            ]);
+        } else {
+            SQL("UPDATE ".self::TABLE_NAME." SET attempts=:attempts, penalty_expiration=:penalty_expiration WHERE ip=:ip AND username=:username", [
+                ":ip"=>$this->request->getIpAddress(),
+                ":username"=>$this->userName,
+                ":attempts"=>$this->attempts,
+                ":penalty_expiration"=>$this->penaltyExpiration
+            ]);
+        }
+    }
+}
