061942fba0e70a4dcba11f50cfbc88d24a46050e
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2021-08-15 07:08:44 +0300

    bugfix

--- index.php
+++ index.php
@@ -12,12 +12,6 @@ define("ENVIRONMENT", $environment);
 new Lucinda\STDERR\FrontController("stderr.xml", ENVIRONMENT, __DIR__, new Lucinda\Project\EmergencyHandler());
 
 // handles STDOUT flow
-if (empty($argv)) {
-    $object = new Lucinda\STDOUT\FrontController("stdout.xml", new Lucinda\Project\Attributes());
-    $object->addEventListener(Lucinda\STDOUT\EventType::REQUEST, Lucinda\Project\EventListeners\Error::class);
-    $object->run();
-} else {
-    $object = new Lucinda\ConsoleSTDOUT\FrontController("stdout.xml", new Lucinda\ConsoleSTDOUT\Attributes());
-    $object->addEventListener(Lucinda\STDOUT\EventType::REQUEST, Lucinda\Project\EventListeners\Error::class);
-    $object->run();
-}
+$object = new Lucinda\STDOUT\FrontController("stdout.xml", new Lucinda\Project\Attributes());
+$object->addEventListener(Lucinda\STDOUT\EventType::REQUEST, Lucinda\Project\EventListeners\Error::class);
+$object->run();
