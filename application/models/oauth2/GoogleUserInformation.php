07c3fc65f2dd94b1131f627e939c7b10784fff46
Lucian Gabriel Popescu <lucian@hliscorp.com>
2021-10-25 20:41:11 +0300

    Google OAuth2

--- application/models/oauth2/GoogleUserInformation.php
+++ application/models/oauth2/GoogleUserInformation.php
@@ -3,9 +3,8 @@ require_once("AbstractUserInformation.php");
 
 class GoogleUserInformation extends AbstractUserInformation {
 	public function __construct($info) {
-		$this->id = $info["id"];
-		$this->name = $info["displayName"];
-		$this->email = $info["emails"][0]["value"];
-		// gender
+	        $this->id = $info["id"];
+        	$this->name = $info["name"];
+	        $this->email = $info["email"];
 	} 
 }
