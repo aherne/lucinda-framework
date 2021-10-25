cd07f1eca4d6f3c028e3484490faa155cf25eb43
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2021-10-25 20:41:11 +0300

    Google OAuth2

--- application/models/oauth2/GoogleSecurityDriver.php
+++ application/models/oauth2/GoogleSecurityDriver.php
@@ -7,8 +7,8 @@ require_once("GoogleUserInformation.php");
  */
 class GoogleSecurityDriver extends AbstractSecurityDriver implements OAuth2Driver {
 	// login-related constants
-	const SCOPES = array("https://www.googleapis.com/auth/plus.login","https://www.googleapis.com/auth/plus.profile.emails.read");
-	const RESOURCE_URL = "https://www.googleapis.com/plus/v1/people/me";
+	const SCOPES = array("https://www.googleapis.com/auth/userinfo.email","https://www.googleapis.com/auth/userinfo.profile");
+	const RESOURCE_URL = "https://www.googleapis.com/oauth2/v1/userinfo";
 
 	/**
 	 * {@inheritDoc}
