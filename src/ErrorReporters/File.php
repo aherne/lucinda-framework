b02f1ace045a148dbda9a0db6cdc9593e4a1385d
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2022-05-23 07:40:18 +0300

    removed superglobals

--- src/ErrorReporters/File.php
+++ src/ErrorReporters/File.php
@@ -2,13 +2,12 @@
 
 namespace Lucinda\Project\ErrorReporters;
 
+use Lucinda\Framework\LoggingRequestInformation;
 use Lucinda\Logging\Driver\File\Logger as FileLogger;
 use Lucinda\Logging\Logger;
 use Lucinda\Framework\AbstractReporter;
 use Lucinda\MVC\ConfigurationException;
 use Lucinda\Logging\LogFormatter;
-
-require_once dirname(__DIR__, 2)."/helpers/getRequestInformation.php";
 
 /**
  * Logs throwable into file, whose details may vary according to development environment.
@@ -33,9 +32,10 @@ class File extends AbstractReporter
             throw new ConfigurationException("Attribute 'format' is mandatory for 'file' tag");
         }
 
+        $loggingRequestInfo = new LoggingRequestInformation();
         return new FileLogger(
             $rootFolder."/".$filePath,
-            new LogFormatter($pattern, getRequestInformation()),
+            new LogFormatter($pattern, $loggingRequestInfo->getRequestInformation()),
             (string) $this->xml["rotation"]
         );
     }
